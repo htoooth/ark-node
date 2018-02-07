94d8f37e2817c381693e0986d88596177336acad
htoooth <ht.anglenx@gmail.com>
2018-02-07 22:28:34 +0800

    fix

--- tasks/createAddress.js
+++ tasks/createAddress.js
@@ -4,6 +4,7 @@ const phrases = ['biso1', 'biso2', 'biso3', 'biso4', 'biso5', 'biso6', 'biso7']
 
 function generate(phrase) {
   const keys = ark.crypto.getKeys(phrase)
+  return ark.crypto.getAddress(keys.publicKey)
 }
 
 function main() {
